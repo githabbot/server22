002e4d02463b21cc566cc7602457291da7c9d5f3
githabbot <m.githabbot@gmail.com>
2016-04-10 22:59:53 +0430

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -4,46 +4,46 @@ function run(msg, matches)
 
 local mods = [[ 
 راهنما فارسی مدیران :
-@Motion_Graphics_boy
+@xXDangerXx
 
 !kick [Username | ID | Reply]
-
+اخراج فرد
 !ban [username | ID | Reply]
-
+بن کردن فرد
 !unban [Username | ID | Reply]
-
+آن بن کردن فرد
 !banlist
-
+لیست بن شده ها
 !id [Username | Id | Reply]
-
+ایدی
 !res @username
-
+اطلاعات یوزرنیم
 !res [Reply]
-
+اطلاعات با ریپلی
 !lock [ads | name | tag | leave | badw | member | chat | farsi | bots]
-
+قفل (تبلیغ|تگ|خرئج|فحش|اعضا|چت|فارسی|ربات ها(
 !unlock [ads | name | tag | leave | badw | member | chat | farsi | bots]
-
+بازکردن قفل 
 !all
-
+تمامی اطلاعات گروه
 !addsudo
-
+اضافه کردن ادمین اصلی به ربات
 !set[photo | name]
-
+تنظیم عکس|نام
 !set [rules | about]
-
+تنظیم قوانین|درباره
 !tagall [MSG]
-
+تگ کردن همه
 !who
-
+کیست؟
 !filter [+-?] [word]
-
+فیلتر کردن کلمه یا حذف فیلتر
 !set [value] [MSG]
-
+تنظیم پیام ارزش
 !info [Username | ID | Reply]
-
+اطلاعات فرد
 !plugins [-+] (Plug Name) chat
-
+اضافه و یا حذف پلاگین
 ==========================
 به علاوه تمامی دستورات رنک :
 member
@@ -65,25 +65,26 @@ member
  local admin = [[ 
  
  راهنمای فارسی ادمین ها :
- @boomrange @Motion_Graphics_boy
+ @boomrange @xXDangerXx
  
  !banall [Reply | ID | Username]
- 
+ بن کردن از تمامی گروه ها
  !unbanall [ID]
- 
+ آنبن کردن از تمامی گروه ها
  !add 
- 
+ اضافه کردن گروه
  !rem
- 
+ حذف گروه
  !leave
- 
+ خروج ربات
  !setowner [ID | Reply]
- 
+ تنظیم صاحب
  !kill chat [ID]
- 
+ پاک کردن چت
  !bc [GP-ID] [MSG]
- 
+ سند تو آل
  !all [GP-ID]
+ تمامی اطلاعات گروه
  ==========================
  به علاوه تمامی دستورات رنک های :
  owner
@@ -108,17 +109,18 @@ member
  local owner = [[
  
  راهنمای فارسی صاحبان گروه :
- @Motion_Graphics_boy
+ @xXDangerXx
  
  !promote [Reply | ID | Username]
- 
+ اضافه کردن مدیر
  !demote [Reply | ID | Username]
- 
+ حذف مدیر
  !setowner [ID | Reply]
- 
+ تغیر صاحب گروه
  !addsudo
- 
+ اضافه کردن ادمین
  !setrank [Reply | ID | Username]
+ تنظیم مقام
  ==========================
  به علاوه تمامی دستورات رنک های :
  mod
@@ -141,35 +143,36 @@ member
  local member = [[
  
  راهنمای فارسی عضو ها :
- @Motion_Graphics_boy
+ @xXDangerXx
  
  !calc [formula]
- 
+ ماشین حساب
  !get [Value]
- 
+ دریافت اطلاعات
  !filterlist
- 
+ لیست فیلتر شده ها
  !info
- 
+ اطلاعات
  !id
- 
+ دریافت ایدی
  !me
- 
+ مقام شما
  !voice [MSG]
- 
+ تبدیل متن به صدا
  !time
- 
+ زمان
  !time [Area]
- 
+ زمان در منطقه جغرافیایی مشخص
  !tex [Msg]
- 
+ متن
  !feedback [msg]
- 
+ فرستادن نظرات خود
  !share
- 
+ شیر کردن شماره ربات
  !boomrange
- 
+ اطلاعات سازنده
  !sticker [TXT]
+ تبدیل متن به استیکر 
  ==========================
  جهت دریافت راهنمای رنک های دیگر میتوانید از دستورات زیر استفاده کنید :
  
@@ -187,30 +190,30 @@ member
  local sudo = [[
  
  راهنما فارسی سودو ها :
- @Motion_Graphics_boy
+ @xXDangerXx
  
  !addadmin [ID | Username]
- 
+ اضافه کردن ادمین
  !removeadmin [ID | Username]
- 
+ حذف ادمین
  !creategroup [Name]
- 
+ ساخت گروه
  !up [Name.format] [Text]
- 
+ در حال کار
  !dl [name.format]
- 
+ دانلود
  !echo> [name.format] [Text]
- 
+ تکرار متن
  !invite [Username | ID | Reply]
- 
+ دعوت
  !stats bot
- 
+ آمار ربات
  !plugins 
- 
+ پلاگین ها
  !plugins [+-] [Plugname]
- 
+ اضافه و یا حذف پلاگین
  !reload
- 
+ بازنگری پلاگین ها
  ==========================
  به علاوه دسترسی به تمامی دستورات رنک های دیگر
  ==========================
