77df4ffc1777b73eabb4ace05a5c3ee86d0be645
githabbot <m.githabbot@gmail.com>
2016-04-10 23:40:53 +0430

    Update copyright.lua

--- plugins/copyright.lua
+++ plugins/copyright.lua
@@ -1,14 +1,14 @@
 do
 
 local function run(msg, matches)
-  if matches[1]:lower() == 'telemanager' or 'telemanagerplus' then --change this with anything you want
+  if matches[1]:lower() == 'boomrange' or 'telemanagerplus' then --change this with anything you want
     send_document(get_receiver(msg), "./data/me/telemanager.webp", ok_cb, false) --put here sticker location(if you want)
       return [[ 
   
-TeleManagerPlus (Open Source) V 3.0.0
+boomrange (Open Source) V 3.0.0
     Advanced ManagerBot Based On TeleSeed
     
-    Edited By : @Motion_Graphics_boy
+    Edited By : @xXDangerXx
     
   
     Speacial Thanks To My friends :
